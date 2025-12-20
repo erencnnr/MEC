1e312d4e95705148de2a798105776fe100040e92
Eren Çınar <eren.cinar@uzmancrm.com>
2025-12-20 13:12:26 +0300

    Add project files.

---
+++ MEC.AssetManagementUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MEC.AssetManagementUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
