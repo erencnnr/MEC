274507829999aca1328930a1f89dd5a367985b34
Eren Çınar <eren.cinar@uzmancrm.com>
2025-12-26 11:52:10 +0300

    MEC.WebAPI project created

---
+++ MEC.WebAPI/Controllers/ImageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MEC.WebAPI.Models;
+
+namespace MEC.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+        [HttpPost(Name = "UploadImage")]
+        public IActionResult UploadImage(ImageRequestModel request)
+        {
+            ImageResponseModel response = new ImageResponseModel(); 
+            response.Success = true;
+            response.Message = "yüklenen dosya " + request.fileName;
+
+            return Ok(response);
+        }
+    }
+}
---
+++ MEC.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MEC.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "test")]
+        public string Get()
+        {
+            return "Ok";
+        }
+    }
+}
