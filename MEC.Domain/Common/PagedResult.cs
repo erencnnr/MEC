de36480de0f664c162cfd5a894116c4e6c9d3033
Eren Çınar <eren.cinar@uzmancrm.com>
2026-01-12 14:35:52 +0300

    paging added to employee list

---
+++ MEC.Domain/Common/PagedResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEC.Domain.Common
+{
+    public class PagedResult<T>
+    {
+        public List<T> Results { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+        public int PageSize { get; set; }
+        public int RowCount { get; set; }
+
+        public PagedResult()
+        {
+            Results = new List<T>();
+        }
+    }
+}
